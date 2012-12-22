f2917f786cff2df0fa5937db33cab98fd42c1aa0
Josh Dzielak <jdzielak@gmail.com>
2012-12-21 22:41:10 -0800

    Disconnect the twitter stream if the browser disconnects.

--- web.js
+++ web.js
@@ -4,17 +4,22 @@ var io = require('socket.io').listen(parseInt(process.env.PORT)),
 io.set("origins", process.env.ALLOWED_ORIGINS)
 io.set("log level", 2)
 io.sockets.on("connection", function(socket) {
+  var tweetStream;
   socket.on("userstream", function(data) {
-    var twit = new twitter({
+    var twitterClient = new twitter({
       consumer_key: process.env.CONSUMER_KEY,
       consumer_secret: process.env.CONSUMER_SECRET,
       access_token_key: data.token,
       access_token_secret: data.secret
     });
-    twit.stream('user', {}, function(stream) {
-      stream.on('data', function(tweet) {
+    twitterClient.stream('user', {}, function(_tweetStream) {
+      tweetStream = _tweetStream
+      tweetStream.on('data', function(tweet) {
         socket.emit('tweet', tweet)
       })
     })
+  })
+  socket.on("disconnect", function() {
+    tweetStream.destroy()
   })
 })
